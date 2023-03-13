2e6be475102797e2cfeb1d5b23d7c2643e3a54c9
dev-weirdo <dev_weirdo@protonmail.com>
2023-03-13 21:44:27 +0600

    Navbar modified

--- src/components/shared/navbar/Navber.js
+++ src/components/shared/navbar/Navber.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import Logo from '../../../Assets/BAnner.jfif'
 import { Link } from 'react-router-dom';
 
 const Navber = () => {
@@ -11,7 +10,7 @@ const Navber = () => {
     return (
         <div className='text-white flex justify-between py-5'>
             <div className="navbar bg-base-100">
-                <div className="navbar-start">
+                <div className="navbar-start w-full">
                     <div className="dropdown">
                         <label tabIndex={0} className="btn btn-ghost lg:hidden">
                             <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
@@ -21,9 +20,8 @@ const Navber = () => {
                         </ul>
                     </div>
 
-                    <div className='flex justify-between items-center'>
-                        <img className='w-10' src={Logo} alt="" />
-                        <h1>Shoppable</h1>
+                    <div className='flex justify-center md:justify-start w-full'>
+                        <h1 className='font-bold text-xl -ml-10 md:ml-0 md:text-2xl lg:text-3xl'>SHOPP<span className='text-orangerrr'>ABLE</span></h1>
                     </div>
 
                 </div>
