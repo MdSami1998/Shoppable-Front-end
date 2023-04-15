b2033a2d75624d2e99f4c4271d884c0d022411ab
we1rdo <dev_weirdo@protonmail.com>
2023-04-15 22:33:15 +0600

    added

--- src/components/ProductDetails/ProductDetails.js
+++ src/components/ProductDetails/ProductDetails.js
@@ -1,13 +1,25 @@
 import React from 'react';
+import { useQuery } from 'react-query';
 import { useParams } from 'react-router-dom';
+import Loader from '../shared/Loader/Loader';
 
 const ProductDetails = () => {
-
     const {productId} = useParams();
+
+    const { data: product, isLoading } = useQuery('product', () =>
+        fetch(`http://localhost:5000/products/${productId}`).then(res =>
+            res.json()
+        )
+    )
+
+    if(isLoading){
+        return <Loader />
+    }
 
     return (
         <div>
-            <h2>Product detail{productId}</h2>
+            <h2>Product detail{product._id}</h2>
+            <h2>Product detail{product.name}</h2>
         </div>
     );
 };
